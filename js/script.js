a1c69307d1bcc894486f41b64ca93c2386550b31
vothanhphuc <vothanhphuc17091996@gmail.com>
2022-06-29 22:52:20 +0700

    <update_reponsive_v3>

--- js/script.js
+++ js/script.js
@@ -4,14 +4,24 @@ let body = document.getElementById("body")
 let nav = document.getElementById("na");
 let sec_03 = document.getElementById("sec_3");
 let sec_04 = document.getElementById("sec_4");
+let secNav = document.getElementById("sec_nav");
+let h1 = document.querySelectorAll("h1");
+let p = document.querySelectorAll("p")
+console.log(h1);
 moon.addEventListener("click",function(){
     moon.style.display = "none";
     sun.style.display = "block";
     body.style.backgroundColor = "rgb(0 0 0/80%)";
     nav.style.backgroundColor = "#fff";
-    nav.style.borderRadius = "15%";
     sec_03.style.backgroundColor = "rgb(0 0 0/80%)";
     sec_04.style.backgroundColor = "rgb(0 0 0/80%)";
+    secNav.style.backgroundColor = "rgb(0 0 0/80%)";
+    for(let i=0; i<h1.length-9;i++){
+        h1[i].style.color = "#fff";
+    }
+    for(let i=0; i < p.length-7;i++){
+        p[i].style.color = "#fff";
+    }
 })
 sun.addEventListener("click",function(){
     sun.style.display = "none";
@@ -19,4 +29,11 @@ sun.addEventListener("click",function(){
     body.style.background= "#fff";
     sec_03.style.backgroundColor = "#fff";
     sec_04.style.backgroundColor = "#fff";
+    secNav.style.backgroundColor = "#fff";
+    for(let i=0; i<h1.length-9;i++){
+        h1[i].style.color = "rgb(0, 0, 0)";
+    }
+    for(let i=0; i < p.length-7;i++){
+        p[i].style.color = "rgb(0, 0, 0)";
+    }
 })
